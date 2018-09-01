e67060dbfcae509f4577dc1260dfc84ef685e6d4
Noor Mohamed <noormohamed@juniper.net>
2018-08-31 22:58:07 -0700

    first commit

---
+++ bin/commands-help.sh
@@ -0,0 +1,17 @@
+#
+# List of available commands
+#
+# Copyright (c) 2018.
+# All rights reserved.
+#
+
+printf %s "\
+available commands:
+   disk            create disk volume
+      -d|diskname    sandbox name
+   instance        create instance
+      -i|instance    instance name
+      -z|zone        zone
+   firewall        setup firewall on ports
+      -p|ports       ports to configure
+"
