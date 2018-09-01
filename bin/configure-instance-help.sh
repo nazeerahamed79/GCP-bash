a44eb93d56c237a0c33ae53688c4c256457d8a94
Noor Mohamed <noormohamed@juniper.net>
2018-08-31 23:34:10 -0700

    configure instance - base script

---
+++ bin/configure-instance-help.sh
@@ -0,0 +1,14 @@
+#
+# List of available commands
+#
+# Copyright (c) 2018.
+# All rights reserved.
+#
+
+printf %s "\
+available commands:
+   upgradeos       upgrade os
+   install         install git,nginx
+   mount           create filesystem and mount
+     -m               mount path
+"
