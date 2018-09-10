5b3cbf427f588eae72049016e57177850ae09bb6
Nazeer Ahamed <nazeer_ahamed@hotmail.com>
2018-09-10 10:47:53 -0700

    configure-instance-help1.sh

---
+++ bin/configure-instance-help1.sh
@@ -0,0 +1,16 @@
+bin/bash
+##list of available commands
+#
+# Copyright (c) 2018.
+# All rights reserved.
+#
+
+printf %s "\
+   available commands:
+      upgradeos       	upgrade os
+      change_hostname   change hostname
+        -i		   instance name 
+      install        	install git,nginx
+      mount           	create filesystem and mount
+     	-m                 mount path
+     "
